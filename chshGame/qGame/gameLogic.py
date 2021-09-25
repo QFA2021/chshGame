bb77603b93bc74cba9dc4161cc5f457f170223db
schwarzfischer1776 <schwarzfischer.felix@web.de>
2021-09-25 15:13:59 +0200

    data exchange server and website

---
+++ chshGame/qGame/gameLogic.py
@@ -0,0 +1,2 @@
+def testFunction(t_0, t_1, phi_0, phi_1):
+    return t_0 + t_1 + phi_0 + phi_1 
--- chshGame/qGame/views.py
+++ chshGame/qGame/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
-
+from . import gameLogic 
+import random 
+import json 
 # Create your views here.
 
 def index(request):
     return render(request, 'qGame/index.html')
 
 def gameInterface(request):
-
+    if request.is_ajax():
+        testBit = request.GET.get('t_0', None)
+        parameters = request.GET #in the order of t_0, t_1, phi_0, phi_1
+        print("is ajax", testBit, parameters, parameters.get('t_0'), gameLogic.testFunction(1,1,1,1))
+        prob = gameLogic.testFunction(1,1,1,1)
+        dict = {'prob':prob*random.random()}
+        return HttpResponse(json.dumps(dict), content_type='application/json')
     return render(request, 'qGame/gameInterface.html')
