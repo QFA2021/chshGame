7aa04eb24e2aeb0d1e45e81e918f0a4ba821b1fd
Magnus Kühn <magnus.khn@gmail.com>
2022-01-09 10:19:03 +0100

    Remove python backend

--- chshGame/qGame/gameLogic.py
+++
@@ -1,2 +0,0 @@
-def testFunction(t_0, t_1, phi_0, phi_1):
-    return t_0 + t_1 + phi_0 + phi_1 
--- chshGame/qGame/models.py
+++
@@ -1,4 +0,0 @@
-from django.db import models
-
-# Create your models here.
-#WHen inclduing models we need to include within the Installe d APps in the settings 
